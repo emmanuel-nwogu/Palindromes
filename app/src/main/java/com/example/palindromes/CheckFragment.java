e2c7600ba25d549814730ca7e15b22d304129c5a
Emmanuel Nwogu <nwoguositadinma@gmail.com>
2020-08-31 21:35:21 -0500

    Migrated project to androidX to accommodate Firebase.

--- app/src/main/java/com/example/palindromes/CheckFragment.java
+++ app/src/main/java/com/example/palindromes/CheckFragment.java
@@ -2,12 +2,11 @@ package com.example.palindromes;
 
 import android.graphics.Rect;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
-import android.support.v4.app.Fragment;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
 import android.text.Editable;
 import android.text.TextWatcher;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
--- app/src/main/java/com/example/palindromes/ExploreFragment.java
+++ app/src/main/java/com/example/palindromes/ExploreFragment.java
@@ -1,15 +1,13 @@
 package com.example.palindromes;
 
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
-import android.support.v4.app.Fragment;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
 import android.widget.ListView;
-import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.Collections;
